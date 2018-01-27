53cea799cbb81b701b46b6a7783e28bc5c72f4e5
Jeremy Volkman <volkman@google.com>
2018-01-27 15:56:40 -0500

    Take version as parameter

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
-docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd):/data homeassistant/amd64-builder --amd64 -t . --homeassistant 0.61 -d jvolkman
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd):/data homeassistant/amd64-builder --amd64 -t . --homeassistant $1 -d jvolkman
